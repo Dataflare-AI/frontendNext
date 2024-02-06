027466f3a9ffe4f3cd25e9b3702fadd482bd2a73
dudubarbaro <eduardobarbaro.ifc@gmail.com>
2024-02-06 13:58:02 -0300

    Refactor session handling in dashboard layout component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,23 +5,10 @@ import styles from "../ui/dashboard/dashboard.module.css";
 import Footer from "../ui/dashboard/footer/footer";
 import "src/app/globals.css";
 import SessionProvider from "../SessionProvider";
+import { useSession } from "next-auth/react";
 
-// Defina o tipo de sessão no formato esperado pelo SessionProvider
-interface NextAuthSession {
-  name?: string | null;
-  email?: string | null;
-  image?: string | null;
-  expires: Date; // Adicione a propriedade 'expires' ao tipo
-}
-
-export default function RootLayout({ children }: { children: ReactNode }) {
-  // Crie uma sessão compatível com o tipo esperado
-  const session: NextAuthSession = {
-    name: "User Name",
-    email: "user@example.com",
-    image: "https://example.com/avatar.png",
-    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 dias em milissegundos
-  };
+export default function Layout({ children }) {
+  const { data: session, status } = useSession();
 
   return (
     <SessionProvider session={session}>
