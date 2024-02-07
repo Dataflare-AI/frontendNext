7874057f90cfa542dcd29667f283d21039bd0316
dudubarbaro <eduardobarbaro.ifc@gmail.com>
2024-02-07 16:21:47 -0300

    Refactor importFiles.tsx to conditionally render Dialog component

--- src/app/ui/importFiles/importFiles.tsx
+++ src/app/ui/importFiles/importFiles.tsx
@@ -355,42 +355,44 @@ function ImportFiles() {
         )}
       </form>
 
-      <Dialog>
-        <DialogTrigger>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
-            />
-          </svg>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Informações do Arquivo</DialogTitle>
-            <DialogDescription>
-              {totalRows && (
-                <div className="mt-4">
-                  <p className="">Linhas: {totalRows}</p>
-                </div>
-              )}
+      {excelFile && (
+        <Dialog>
+          <DialogTrigger>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+              />
+            </svg>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Informações do Arquivo</DialogTitle>
+              <DialogDescription>
+                {totalRows && (
+                  <div className="mt-4">
+                    <p className="">Linhas: {totalRows}</p>
+                  </div>
+                )}
 
-              {fileSize && (
-                <div className="mt-2">
-                  <p className="">Tamanho: {fileSize}KB</p>
-                </div>
-              )}
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
+                {fileSize && (
+                  <div className="mt-2">
+                    <p className="">Tamanho: {fileSize}KB</p>
+                  </div>
+                )}
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      )}
 
       <LoadingModal
         visible={(isLoading || isSheetLoading) && selectedSheet !== null}
